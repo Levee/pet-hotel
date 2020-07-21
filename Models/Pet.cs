e8e42a07bf827a63cbbe7bc5001a70ff6c6d6a3c
Blaine Booher <blaine@emergingprairie.com>
2020-07-21 00:44:44 -0500

    initial checkin for pet hotel assignment

---
+++ Models/Pet.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace pet_hotel
+{
+    public enum PetBreedType {}
+    public enum PetColorType {}
+    public class Pet
+    {}
+}
